320a4b5e567a1dc56abf66c044d76adbb9f0c2fb
yoi3.0 <dkurpachenko@gmail.com>
2019-10-03 20:26:21 +0300

    Updates

---
+++ precache-manifest.6987a2273a9fca1e3e8210a11f13c298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ac17e283ad7c51c639cb6605907463",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd5391f8baf4e6d3a199",
+    "url": "./static/css/2.01749317.chunk.css"
+  },
+  {
+    "revision": "abba55a344fbe4ad1084",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "cd5391f8baf4e6d3a199",
+    "url": "./static/js/2.7332d8ab.chunk.js"
+  },
+  {
+    "revision": "abba55a344fbe4ad1084",
+    "url": "./static/js/main.fafb5077.chunk.js"
+  },
+  {
+    "revision": "ae8d0d27979e127d00d5",
+    "url": "./static/js/runtime-main.27a037fe.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
