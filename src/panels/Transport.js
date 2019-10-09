2f2c70dfa807c2a831039ab26dd5b56c9ff8b2d5
yoi3.0 <dkurpachenko@gmail.com>
2019-10-09 18:57:40 +0300

    Info about route get from array beta

--- src/panels/Transport.js
+++ src/panels/Transport.js
@@ -8,42 +8,42 @@ import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
 import Icon24Done from '@vkontakte/icons/dist/24/done';
 
 const routes = [
-	{id: 999, name: "Трамвай А"},
-	{id: 1003, name: "Трамвай 3"},
-	{id: 1006, name: "Трамвай 6"},
-	{id: 1007, name: "Трамвай 7"},
-	{id: 1009, name: "Трамвай 9"},
-	{id: 1010, name: "Трамвай 10"},
-	{id: 1016, name: "Трамвай 16"},
-	{id: 1018, name: "Трамвай 18"},
-	{id: 1019, name: "Трамвай 19"},
-	{id: 1020, name: "Трамвай 20"},
-	{id: 1022, name: "Трамвай 22"},
-	{id: 1023, name: "Трамвай 23"},
-	{id: 1024, name: "Трамвай 24"},
-	{id: 1025, name: "Трамвай 25"},
-	{id: 1027, name: "Трамвай 27"},
-	{id: 1029, name: "Трамвай 29"},
-	{id: 1030, name: "Трамвай 30"},
-	{id: 1036, name: "Трамвай 36"},
-	{id: 1038, name: "Трамвай 38"},
-	{id: 1039, name: "Трамвай 39"},
-	{id: 1040, name: "Трамвай 40"},
-	{id: 1041, name: "Трамвай 41"},
-	{id: 1043, name: "Трамвай 43"},
-	{id: 1045, name: "Трамвай 45"},
-	{id: 1047, name: "Трамвай 47"},
-	{id: 1048, name: "Трамвай 48"},
-	{id: 1049, name: "Трамвай 49"},
-	{id: 1051, name: "Трамвай 51"},
-	{id: 1052, name: "Трамвай 52"},
-	{id: 1055, name: "Трамвай 55"},
-	{id: 1067, name: "Трамвай 57"},
-	{id: 1068, name: "Трамвай 58"},
-	{id: 1060, name: "Трамвай 60"},
-	{id: 1061, name: "Трамвай 61"},
-	{id: 1062, name: "Трамвай 62"},
-	{id: 1100, name: "Трамвай 100"},
+	{id: 999, name: "Трамвай А", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1003, name: "Трамвай 3", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1006, name: "Трамвай 6", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1007, name: "Трамвай 7", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1009, name: "Трамвай 9", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1010, name: "Трамвай 10", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1016, name: "Трамвай 16", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1018, name: "Трамвай 18", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1019, name: "Трамвай 19", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1020, name: "Трамвай 20", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1022, name: "Трамвай 22", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1023, name: "Трамвай 23", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1024, name: "Трамвай 24", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1025, name: "Трамвай 25", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1027, name: "Трамвай 27", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1029, name: "Трамвай 29", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1030, name: "Трамвай 30", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1036, name: "Трамвай 36", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1038, name: "Трамвай 38", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1039, name: "Трамвай 39", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1040, name: "Трамвай 40", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1041, name: "Трамвай 41", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1043, name: "Трамвай 43", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1045, name: "Трамвай 45", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1047, name: "Трамвай 47", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1048, name: "Трамвай 48", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1049, name: "Трамвай 49", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1051, name: "Трамвай 51", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1052, name: "Трамвай 52", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1055, name: "Трамвай 55", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1057, name: "Трамвай 57", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1058, name: "Трамвай 58", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1060, name: "Трамвай 60", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1061, name: "Трамвай 61", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1062, name: "Трамвай 62", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
+	{id: 1100, name: "Трамвай 100", cost: '40', park: 'Трамвайный парк #3, Трамвайный парк #7', sostav:' s'},
 ];
 const osName=platform();
 const IS_PLATFORM_ANDROID = (osName === ANDROID);
@@ -81,11 +81,11 @@ class Transport extends React.Component {
             activeRoute=activeModal[activeModal.length-1];
 			modalHistory = modalHistory.splice(0, modalHistory.indexOf(activeModal) + 1);
 		} else {
-            activeRoute=activeModal-1000;
+            activeRoute=activeModal;
 			modalHistory.push(activeModal);
 			activeModal='tram'
 		}
-		if (activeRoute==-1) activeRoute='A';
+		//if (activeRoute==) activeRoute='A';
 		this.setState({
             activeRoute,
 			activeModal,
@@ -99,6 +99,10 @@ class Transport extends React.Component {
 		return routes.filter(({name}) => name.toLowerCase().indexOf(search) > -1);
 	}
 
+	get infobase () {
+		return routes.filter(({id})=> id===this.state.activeRoute);
+	}
+
 	render()
 	{
 		const modal=(
@@ -106,14 +110,14 @@ class Transport extends React.Component {
 				<ModalPage
 					className='modal-page'
 					id={'tram'}
-					settlingHeight='30' //ono rabotaet ili net? cha za fignya?
+					settlingHeight='30' //ono rabotaet ili net? chto za fignya???
 					onClose={this.modalBack}
 					header={
 						<ModalPageHeader
 							left={IS_PLATFORM_ANDROID && <HeaderButton onClick={this.modalBack}><Icon24Cancel /></HeaderButton>}
 							right={<HeaderButton onClick={this.modalBack}>{IS_PLATFORM_IOS ? 'Готово' : <Icon24Done />}</HeaderButton>}
 						>
-							Трамвай {this.state.activeRoute}
+							Трамвай {this.state.activeRoute-1000>-1? this.state.activeRoute-1000 : (this.state.activeRoute-1000===-1? 'A': ' ') }
 						</ModalPageHeader>
 					}
 				>
@@ -137,23 +141,31 @@ class Transport extends React.Component {
 							</List>
 						</Group>
 						<Group title='Другая информация'>
-							<list>
-								<Cell>
-									<InfoRow title='Стоиимость проезда'>
-										40 рублей
-									</InfoRow>
-								</Cell>
-								<Cell>
-									<InfoRow title='Обслуживающие парки'>
-										Трамвайный парк #3, Трамвайный парк #7
-									</InfoRow>
-								</Cell>
-								<Cell>
-									<InfoRow title='Подвижной состав'>
-										ЛМ-68М2, ЛМ-68М3, ЛМ-99АВН, 71-301, 71-623, 71-623-02, 71-631, 71-631-02
-									</InfoRow>
-								</Cell>
-							</list>
+							{this.infobase.length > 0?
+								<list>
+									{this.infobase.map(infobase =>
+										<Cell key={infobase.id} >
+											<InfoRow title='Стоиимость проезда'>
+												{infobase.cost}
+											</InfoRow>
+										</Cell>)}
+									{this.infobase.map(infobase =>
+										<Cell key={infobase.id} >
+											<InfoRow title='Обслуживающие парки'>
+												{infobase.park}
+											</InfoRow>
+										</Cell>)}
+									{this.infobase.map(infobase =>
+										<Cell key={infobase.id} >
+											<InfoRow title='Подвижной состав'>
+												{infobase.sostav}
+											</InfoRow>
+										</Cell>)}
+								</list>:
+								<inforow className='zaglushka'>
+									О данном маршруте нет информации
+								</inforow>
+							}
 						</Group>
 					</div>
 				</ModalPage>
@@ -174,10 +186,13 @@ class Transport extends React.Component {
 							Маршруты
 						</PanelHeader>
 						<Search value={this.state.search} onChange={this.onChange}/>
-						{this.thematics.length > 0 &&
+						{this.thematics.length > 0 ?
 						<List>
 							{this.thematics.map(thematic => <Cell key={thematic.id} onClick={() => this.setActiveModal(thematic.id)} >{thematic.name}</Cell>)}
-						</List>
+						</List>:
+							<InfoRow className='zaglushka'>
+								Маршрут не найден.
+							</InfoRow>
 						}
 					</div>
 				</Panel>
